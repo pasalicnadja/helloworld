a4f774957f22878ee580b89d251b6d6192988f96
karahasanamer <akarahasan4@etf.unsa.ba>
2019-03-20 09:45:42 +0100

    initial commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label labela;
+
+    public void ispisi(ActionEvent actionEvent) {
+        labela.setText("Hello world!");
+    }
 }
